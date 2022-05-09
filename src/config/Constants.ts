c48ee7879bd1f677c899dfd26defc6b9f040d778
Vitor Dallagnolo <86325265+vitordallagnolo@users.noreply.github.com>
2022-05-09 15:06:09 -0300

    Update Constants.ts

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -1,5 +1,5 @@
 export const config = {
     API_URL: 'http://localhost:3000',
     MONGO_CONNECTION:
-    'mongodb+srv://dallagnolo:NDdwYnTocCQtWlHF@url-shortner.eehhs.mongodb.net/url-shortner?retryWrites=true&w=majority'
+    'mongodb+srv://dallagnolo:<password>@url-shortner.eehhs.mongodb.net/url-shortner?retryWrites=true&w=majority'
 } 
